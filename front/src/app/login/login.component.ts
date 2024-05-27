1bc017076294eb0626b837f59159f4a3d545001f
rosty-git <rostyslavpopadyn@gmail.com>
2024-05-27 18:28:30 +0300

    fix(front): submit post then enter key pressing

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -1,9 +1,7 @@
-// import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
-// import { WebSocketService } from '../web-socket.service';
 import { ApiService } from '../api.service';
 
 @Component({
