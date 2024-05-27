4a1fdfdc1f10f6e5daa2cb2d36612f4023113f71
rosty-git <rostyslavpopadyn@gmail.com>
2024-05-27 17:33:57 +0300

    feat(front): Added logo

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { provideIcons } from '@ng-icons/core';
+import { cssMenuGridR } from '@ng-icons/css.gg';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 import { MessengerComponent } from './messenger/messenger.component';
@@ -16,6 +18,7 @@ import { MessengerComponent } from './messenger/messenger.component';
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  viewProviders: [provideIcons({ cssMenuGridR })],
 })
 export class AppComponent {
   title = 'chat_tool_front';
--- front/src/app/header/header.component.ts
+++ front/src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { NgIconComponent } from '@ng-icons/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
+  imports: [NgIconComponent],
 })
 export class HeaderComponent {}
